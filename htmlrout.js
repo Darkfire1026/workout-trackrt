89389f8b07e0d6e3d8bf2f929f2e84d800aa5a91
Blake pierce <darkfire1026@gmail.com>
2020-02-01 20:28:22 -0600

    the final product in hopes heroku accepts it

---
+++ htmlrout.js
@@ -0,0 +1,19 @@
+//routs to send users to html pages//
+  
+var path = require("path");
+
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
+};
